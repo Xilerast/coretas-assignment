ae98b170ee4e4a66b5c3e269d2c63ae762faf5e4
Xilerast <LaertisD9@gmail.com>
2024-09-14 21:44:33 +0300

    Renamed notes_app to tasks_app

--- tasks_app/api/models/task_model.py
+++ tasks_app/api/models/task_model.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 class Task(models.Model):
-    """The model for the notes"""
+    """The model for the tasks"""
     title = models.TextField(null=False, blank=False, validators=[MinLengthValidator(limit_value=1)])
     description = models.TextField(null=True, blank=True)
     completion_status = models.BooleanField(null=False, blank=False, default=False)
