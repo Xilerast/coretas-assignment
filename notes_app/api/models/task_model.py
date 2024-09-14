5836c61c5b6c84125a1654794dff4739e15868d5
Xilerast <LaertisD9@gmail.com>
2024-09-14 21:43:31 +0300

    Moved api folder inside notes_app folder

--- notes_app/api/models/task_model.py
+++ notes_app/api/models/task_model.py
@@ -3,7 +3,7 @@ from django.core.validators import MinLengthValidator
 
 class Task(models.Model):
     """The model for the notes"""
-    title = models.TextField(null=False, blank=False, validators=MinLengthValidator(limit_value=1))
+    title = models.TextField(null=False, blank=False, validators=[MinLengthValidator(limit_value=1)])
     description = models.TextField(null=True, blank=True)
     completion_status = models.BooleanField(null=False, blank=False, default=False)
 
