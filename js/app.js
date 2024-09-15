33afdd4eb9e94b6eed62d4fe2f45ffd9dad6afe7
Xilerast <LaertisD9@gmail.com>
2024-09-15 16:53:04 +0300

    Moved app.js content to templates.js

--- js/app.js
+++
@@ -1,9 +0,0 @@
-async function registerTemplate() {
-    let registration_template = await (await fetch("../html/register.html")).text();
-    $("#main-content").html(registration_template);
-}
-
-async function loginTemplate() {
-    let login_template = await (await fetch("../html/login.html")).text();
-    $("#main-content").html(login_template);
-}
